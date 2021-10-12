1d374afc51735ba5aa0967319c0d9d3e0653e162
Rheitheillaethor <minegocu3@gmail.com>
2021-10-12 13:24:21 +0200

    Gender selection works!

--- NameGenerator/Program.cs
+++ NameGenerator/Program.cs
@@ -6,7 +6,23 @@ namespace NameGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, choose your gender!");
+            Console.WriteLine("Write m or f");
+            Console.WriteLine("m - male");
+            Console.WriteLine("f - female");
+            string gender = Console.ReadLine();
+            if (gender=="m")
+            {
+                Console.WriteLine("Man");
+            }
+            else if(gender=="f")
+            {
+                Console.WriteLine("Women");
+            }
+            else
+            {
+                Console.WriteLine("You choosed wrong letter");
+            }
         }
     }
 }
